481e030c5474d0b80260592c900026fffeed296a
JoelVStan <joelvstan@gmail.com>
2024-02-19 16:11:30 +0530

    Canvas ready, but stopping development due to personal reasons.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,48 @@
 "use client";
 
+import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSidebar from "@/components/RightSidebar";
+import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+
+import { fabric } from "fabric";
+import { useEffect, useRef } from "react";
 
 export default function Page() {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null) 
+  const fabricRef = useRef<fabric.Canvas | null>(null)
+  const isDrawing = useRef(false)
+  const shapeRef = useRef<fabric.Object | null>(null)
+  const selectedShapeRef = useRef<string | null>('rectangle')
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef })
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      })
+    })
+
+    window.addEventListener("resize", () => {
+      handleResize({ canvas: fabricRef.current })
+    })
+  }, [])
+
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
 
       <section className="flex h-full flex-row">
-        <Live />
+        <LeftSidebar />
+        <Live canvasRef={canvasRef}/>
+        <RightSidebar />
       </section>
       
     </main>
--- components/Live.tsx
+++ components/Live.tsx
@@ -8,7 +8,12 @@ import ReactionSelector from './reaction/ReactionButton';
 import FlyingReaction from './reaction/FlyinReaction';
 import useInterval from '@/hooks/useInterval';
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>
+}
+
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
@@ -139,12 +144,16 @@ const Live = () => {
   }, [])
 
   return (
-    <div onPointerMove={handlePointerMove}
+    <div 
+         id="canvas"
+         onPointerMove={handlePointerMove}
          onPointerLeave={handlePointerLeave}
          onPointerDown={handlePointerDown}
          onPointerUp={handlePointerUp}
          className="h-[100vh] w-full flex justify-center items-center text-center">
-        <h1 className="text-2xl text-white">FigCraft - Figma Clone</h1>
+        
+        <canvas ref={canvasRef}/>
+
 
         {reactions.map((r) => (
             <FlyingReaction 
